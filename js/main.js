983abd194f518f41b2e1e69fee810ac75de98175
Jackie Culbreath <jmc081964@gmail.com>
2018-07-14 15:43:32 -0400

    added variable for all paragraphs and changed 3 css styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-//Remove all images
+// Add Ready function; variable for all paragraphs; change CSS styling for paragraphs
 
-$( 'img' ).remove();
+$(document).ready(function () {
+  var allParagraphs = $( 'p' );
+  allParagraphs.css( {
+    'color': '#fff',
+    'font-size': '20px',
+    'background-color': 'grey'
+  })
+});
