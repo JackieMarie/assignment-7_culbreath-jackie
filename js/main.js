1a943dc5ce74a580afb9421618e1ac46e06a6719
Jackie Culbreath <jmc081964@gmail.com>
2018-07-14 15:35:20 -0400

    added headings variable, changed text of last h2 element

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-//Remove all images
+// Write JQuery ready function; use JQuery to get all the <h2> elements and assign them a variable named headings
+// Use heading variable to change text of the last h2 element
 
-$( 'img' ).remove();
+$(document).ready(function () {
+  var h2Headings = $( 'h2' );
+  h2Headings.last().text( "I'm Jackie Culbreath, and I'm learning JQuery!" )
+});
